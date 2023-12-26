78a01fa15764ee795fc9f2a0b706c1a77b662643
KoblizekXD <janprokupek@centrum.cz>
2023-12-26 14:28:08 +0100

    feat: add basic auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'School Login',
+            id: 'school_login',
+            credentials: {
+                schoolServer: { label: "Škola", type: 'text', placeholder: "spst.cz" },
+                identity: { label: "Email/Identita", type: "text" },
+                password: { label: "Heslo", type: "password" }
+            },
+            async authorize(credentials, req) {
+                return null
+            }
+        })
+    ],
+    pages: {
+        signIn: '/login',
+        signOut: '/logout',
+    }
+})
+
+export { handler as GET, handler as POST }

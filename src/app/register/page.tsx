fb4c090899493f56db77e242d26289c309750675
KoblizekXD <janprokupek@centrum.cz>
2024-01-04 07:36:22 +0100

    feat: registration page(still missing backend)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,12 +2,28 @@
 
 import ActionButton from "@/components/actionbutton";
 import TextInput from "@/components/textinput";
+import TopError from "@/components/toperror";
 import Link from "next/link";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Register() {
-  const [identity, setIdentity] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [domain, setDomain] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [canSubmit, setCanSubmit] = useState(true)
+
+  const handleRegistrationCheck = async (e: FormEvent) => {
+    try {
+      e.preventDefault()
+      setLoading(true);
+      setError('Registrace nebyla dokončena kvůli internímu problém aplikace');
+      setLoading(false);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error)
+    }
+  };
 
   return (
     <main
@@ -16,41 +32,52 @@ export default function Register() {
       }
     >
       <main
-        className={"login-bg w-full h-full flex justify-center items-center"}
+        className={"register-bg w-full h-full flex justify-center items-center"}
       >
+        { error != '' ? <TopError error={error} /> : null }
         <form
+        onSubmit={handleRegistrationCheck}
           className={
             "flex flex-col gap-2 w-1/5 h-1/2 bg-[#181a1f] shadow-xl border border-[#1d2537] rounded *:ml-4 *:mr-4"
           }
         >
           <h1 className={"font-bold text-2xl mt-4"}>Registrace Školy</h1>
-          <p className={""}>
+          <p className={'text-justify'}>
             Děkujeme za zájem o náš systém, mějte prosím na paměti, že aplikace je pořád v
             rané fázi vývoje a může obsahovat chyby. Prosíme vás tedy, abyste nám tyto chyby hlásili na GitHubu.
           </p>
           <TextInput
-            onChange={(e) => setIdentity(e.target.value)}
+            onChange={(e) => {
+              setDomain(e.target.value)
+              if (e.target.value.length > 0) {
+                setCanSubmit(false);
+              } else setCanSubmit(true)
+            }}
             name="identity"
-            className={"w-[90%]"}
-            placeholder="Identita"
+            placeholder="Doména školy"
           />
           <TextInput
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-            className={"w-[90%]"}
-            placeholder="Heslo"
+            onChange={(e) => { 
+              setEmail(e.target.value) 
+              if (e.target.value.length > 0) {
+                setCanSubmit(false);
+              } else setCanSubmit(true)
+            }}
+            name="email"
+            placeholder="E-mail hlavního správce"
           />
+          <p className={'text-center'}>
+            Na e-mail Vám zašleme potvrzovací zprávu,
+            poté budete moct pokračovat ve vytváření profilu
+            vaší školy.
+          </p>
           <div className={"mb-4 mt-auto flex flex-col gap-2"}>
             <Link href="/" className={"text-[#0066ff]"}>
               Domů
             </Link>
-            <ActionButton
-            onClick={() => {}}
-              className={"bg-[#0066ff] border-[0] w-full"}
-            >
-              Pokračovat
-            </ActionButton>
+            <button disabled={loading || canSubmit} type="submit" className={`disabled:opacity-50 bg-[#0066ff] border-[0] w-full rounded text-white text-center py-3 px-3`}>
+              { loading ? 'Načítání...' : 'Pokračovat'}
+            </button>
           </div>
         </form>
       </main>

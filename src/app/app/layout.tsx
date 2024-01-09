f22efa0e4c2f0ecadd823df451f6d69ff3ffbc90
KoblizekXD <janprokupek@centrum.cz>
2024-01-09 07:11:44 +0100

    fix: fix non-working redirections

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,53 +1,9 @@
-'use client'
-
-import Navbar from "@/components/app/navbar";
-import NavItem from "@/components/app/navitem";
 import { getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
-import { usePathname } from "next/navigation";
 import { config } from "../api/auth/[...nextauth]/route";
+import AppLay from "@/components/alt/applayout";
 
 export default async function AppLayout({children}: {children: React.ReactNode}) {
-  const pathname = usePathname();
   const session = await getServerSession(config)
 
-  const select = (path: string) => {
-    if (path == pathname) return true;
-    else return false;
-  }
-
-  return (
-    <main
-      className={
-        "text-white overflow-auto w-full h-full fixed bg-[#161718] flex flex-row"
-      }
-    >
-      <Navbar>
-        <NavItem href="/app/home" icon="home">
-          {JSON.stringify(session, null, 2)}
-        </NavItem>
-        <NavItem href="/app/home" icon="home" selected={select('/app/home')}>
-          Domů
-        </NavItem>
-        <NavItem href="/app/timetable" selected={select('/app/timetable')} icon="view_list" materialClass="material-symbols-outlined">
-          Rozvrh
-        </NavItem>
-        <NavItem href="/app/grading" icon="checklist" selected={select('/app/grading')}>
-          Známky
-        </NavItem>
-        <NavItem href="/app/absence" icon="person" selected={select('/app/absence')}>
-          Absence
-        </NavItem>
-        <NavItem href="/app/stream" icon="description" selected={select('/app/stream')}>
-          Soubory
-        </NavItem>
-        <div className="mt-auto">
-          <NavItem href="/app/options" icon="settings" selected={select('/app/options')}>
-            Nastavení
-          </NavItem>
-        </div>
-      </Navbar>
-      {children}
-    </main>
-  );
+  return <AppLay children={children} session={session} />
 }
---
+++ src/components/alt/applayout.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Navbar from "@/components/app/navbar";
+import NavItem from "@/components/app/navitem";
+import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
+
+interface LayoutProps {
+  session: any;
+  children: React.ReactNode;
+}
+
+export default function AppLay({session, children} : LayoutProps) {
+  const pathname = usePathname();
+
+  const select = (path: string) => {
+    if (path == pathname) return true;
+    else return false;
+  };
+
+  return (
+    <main
+      className={
+        "text-white overflow-auto w-full h-full fixed bg-[#161718] flex flex-row"
+      }
+    >
+      <Navbar>
+        <NavItem href="/app/home" icon="home" selected={select("/app/home")}>
+          Domů
+        </NavItem>
+        <NavItem
+          href="/app/timetable"
+          selected={select("/app/timetable")}
+          icon="view_list"
+          materialClass="material-symbols-outlined"
+        >
+          Rozvrh
+        </NavItem>
+        <NavItem
+          href="/app/grading"
+          icon="checklist"
+          selected={select("/app/grading")}
+        >
+          Známky
+        </NavItem>
+        <NavItem
+          href="/app/absence"
+          icon="person"
+          selected={select("/app/absence")}
+        >
+          Absence
+        </NavItem>
+        <NavItem
+          href="/app/stream"
+          icon="description"
+          selected={select("/app/stream")}
+        >
+          Soubory
+        </NavItem>
+        <div className="mt-auto">
+          <NavItem
+            href="/app/options"
+            icon="settings"
+            selected={select("/app/options")}
+          >
+            Nastavení
+          </NavItem>
+        </div>
+      </Navbar>
+      {children}
+    </main>
+  );
+}

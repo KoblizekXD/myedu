2835f3045a6168ccf90670a4b52e9db1234beb85
KoblizekXD <janprokupek@centrum.cz>
2024-01-19 19:20:37 +0100

    feat: remove floating navbar

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -9,9 +9,9 @@ interface NavbarProps {
 export default function Navbar({ children }: NavbarProps) {
 
   return (
-    <nav className={'flex border-[#313537] my-2 ml-2 border rounded-lg flex-col w-[15%]'}>
+    <nav className={'flex border-[#313537] border-r flex-col w-[15%]'}>
       <div className={'flex justify-center flex-col items-center my-4'}>
-        <h1 className={'text-2xl font-semibold'}>MyEdu</h1>
+        <h1 className={'text-2xl font-bold'}>My Edu</h1>
         <h2>verze 0.0.1</h2>
       </div>
       {children}
--- src/components/app/separator.tsx
+++ src/components/app/separator.tsx
@@ -1,6 +1,6 @@
 
 export default function Separator() {
   return (
-    <div className={'w-[90%] border-t-[#313537] border-t rounded ml-[5%]'} />
+    <div className={'w-[90%] self-center border-t-[#313537] border-t rounded'} />
   )
 }

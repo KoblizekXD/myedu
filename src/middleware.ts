7a434e7ce9b2b150630a1dc4306d07bb769dde14
KoblizekXD <janprokupek@centrum.cz>
2024-01-23 20:39:54 +0100

    fix: fix middleware not working

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createMiddleware } from 'next-easy-middlewares';
-import { MiddlewareConfig } from "next/dist/build/analysis/get-page-static-info";
-import { checkPermissions, fetchSession } from "./util/util";
+import { createMiddleware } from "next-easy-middlewares";
 
-export { default } from "next-auth/middleware"
+export function checkPermissions(permissions: 'admin' | 'student' | 'teacher', session: any): boolean {
+  if (permissions === 'student') {
+    return true
+  } else if (permissions === 'teacher' && (session.user.teacher != null || session.user.admin != null)) {
+    return true
+  } else if (permissions === 'admin' && session.user.admin) {
+    return true
+  } else return false
+}
+
+export { default } from "next-auth/middleware";
 
 const middlewares: Record<string, any> = {
-  '/app/admin/:path*': async (request: NextRequest) => {
-    const session = await fetchSession()
-    if (session && checkPermissions('admin', session))
+  "/app/admin/:path*": async (request: NextRequest) => {
+    const session = await (await fetch(
+      new URL("/api/auth/session", request.url),
+      { headers: Object.fromEntries(request.headers) }
+    )).json();
+    if (session && checkPermissions("admin", session))
       return NextResponse.next();
-    else
-      return NextResponse.redirect('/app/home?error=unauthorized');
+    else return NextResponse.redirect("/app/home?error=unauthorized");
   },
-}
+};
 
 export const middleware = createMiddleware(middlewares);
 
 export const config = {
-  matcher: ['/app/:path*']
-}
+  matcher: ["/app/:path*"],
+};

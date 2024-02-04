8c0c6ca14821a1f8c02e344b269af3160531f442
KoblizekXD <janprokupek@centrum.cz>
2024-02-04 21:46:17 +0100

    feat: begin subject editor

--- src/components/alt/newsubject.tsx
+++ src/components/alt/newsubject.tsx
@@ -17,6 +17,7 @@ export default function NewSubject({teacherMap}: NewClassProps) {
   const [teacherId, setTeacherId] = useState('')
   const [message, setMessage] = useState('')
   const [error, setError] = useState('')
+  const [showSubjectEditor, setShowSubjectEditor] = useState(false)
 
   async function createNewClass(e: FormEvent) {
     e.preventDefault()
@@ -45,8 +46,8 @@ export default function NewSubject({teacherMap}: NewClassProps) {
     <main className={'mx-6 my-6 flex flex-col gap-2 w-full'}>
       {message && <TopInfo message={message} />}
       {error && <TopError error={error} />}
-      <h1 className={'font-extrabold text-4xl'}>Vytvořit novou třídu</h1>
-      <Link href={'/app/admin/classes'} className={'text-blue-500 items-center flex text-lg'}>
+      <h1 className={'font-extrabold text-4xl'}>Vytvořit nový předmět</h1>
+      <Link href={'/app/teacher/subjects'} className={'text-blue-500 items-center flex text-lg'}>
         <span className="material-icons">
           arrow_back
         </span>
@@ -58,6 +59,11 @@ export default function NewSubject({teacherMap}: NewClassProps) {
           <TextInput onChange={(e) => setName(e.target.value)} name="class_name" placeholder='Jméno třídy' className={'w-1/6'} />
           <h2 className={'text-xl font-semibold'}>Zvolte učitele</h2>
           <SelectionBox onSelect={i => setTeacherId(teacherMap.get(i) || '')} text={'Zvolit učitele'} items={[...teacherMap.keys()]} />
+          <h2 className={'text-xl font-semibold'}>Zvolte třídu</h2>
+          <SelectionBox onSelect={i => {
+            setShowSubjectEditor(true)
+          }} text={'Zvolit učitele'} items={[...teacherMap.keys()]} />
+          <SubjectEditor claimed={['1x1']} />
           <button
               type="submit"
               className={`disabled:opacity-50 bg-[#0066ff] border-[0] w-1/6 rounded text-white text-center py-3 px-3`}
@@ -67,5 +73,35 @@ export default function NewSubject({teacherMap}: NewClassProps) {
         </form>
       </div>
     </main>
+  )
+}
+
+function SubjectEditor({claimed = []}: {claimed: string[]}) { // claimed format: 1x1 = monday, first hour
+  return (
+    <>
+      <h1 className={'text-xl font-semibold'}>Editor hodin</h1>
+      <div className={'border stdborder w-1/2 h-1/2 p-2 rounded grid grid-rows-5 grid-cols-8 gap-2 *:flex *:justify-center *:py-2'}>
+        {
+          Array(5).fill(1).map((e, i) => {
+            return (Array(8).fill(1).map((e, j) => {
+              const current = [i+1, j+1].join('x')
+              if (claimed.includes(current)) {
+                return (
+                  <div className={'border-sky-500 p-1 rounded text-white items-center border-collapse select-none border text-center'}>
+                    {current}
+                  </div>
+                )
+              } else {
+                return (
+                  <div className={'border select-none text-[#313537] items-center stdborder rounded border-dashed'}>
+                    Prázdné
+                  </div>
+                )
+              }
+            }))
+          })
+        }
+      </div>
+    </>
   )
 }
